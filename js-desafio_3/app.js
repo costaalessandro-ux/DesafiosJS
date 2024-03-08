5d6f1e1cb38cdbaa75b9657f86105ee940fce234
Alessandro <alessandrocosta103@gmail.com>
2024-03-08 11:15:23 -0300

    codando desafios

--- js-desafio_3/app.js
+++ js-desafio_3/app.js
@@ -5,6 +5,7 @@ function imc(altura, peso){
 }
 console.log(`O IMC do paciente é: `,imc(1.80, 100));
 
+
 /*Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.*/
 // IMPLEMENTAÇÃO ERRADA !
 function calcFatorial(fatorial){
@@ -24,12 +25,22 @@ function perimetro(altura, largura){
 }
 perimetro(3,400);
 
-//Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
-function perimetroCircular(){
 
+//Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+function perimetroCircular(raio){
+ let perimetro = 2 * 3.14 * raio;
+ let area = 3.14 * (Math.pow(raio,2));
+ console.log(`Aréa circular: ${area}`);
+ console.log(`Perimetro circular: ${perimetro}`);
 }
+perimetroCircular(100);
+
 
 //Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
-function tabuada(){
-
+function tabuada(numero){
+    for(i = 1; i <= 10; i++){
+        result = numero * i;
+        console.log(`${numero} X ${i} = ${result}`);
+    }
 }
+tabuada(2);
