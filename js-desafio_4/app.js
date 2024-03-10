8349a7ced7504d473cd60e6370a6f68375c3de54
Alessandro <alessandrocosta103@gmail.com>
2024-03-10 01:50:47 -0300

    finaliza desafio

---
+++ js-desafio_4/app.js
@@ -0,0 +1,21 @@
+/*Crie uma lista vazia, com o nome listaGenerica*/
+let listaGenerica = [];
+console.log(listaGenerica);
+
+/*Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes
+ elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.*/
+ let linguagensDeProgramacao = ['JavaScript','C','C++','Kotlin','Python'];
+ console.log(linguagensDeProgramacao);
+ /*Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.*/
+linguagensDeProgramacao.push('Java','Ruby','GoLang');
+console.log(linguagensDeProgramacao);
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.*/
+let listaNome = ['','',''];
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.*/
+
+
+
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o último elemento.*/
